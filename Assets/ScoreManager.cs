68757e740f3d74ec67916dc88388609e64cf89d0
Sumire Kikuta <paddyfield815@gmail.com>
2018-07-02 18:26:38 +0900

    Lesson5課題

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+	
+	private int score = 0;
+    private Text scoreLabel;
+
+    void Start()
+    {
+        // 「Text」コンポーネントにアクセスして取得する（ポイント）
+        scoreLabel = this.gameObject.GetComponent<Text>();
+        scoreLabel.text = "Score " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreLabel.text = "Score " + score;
+
+    }
+        // スコアを加算するメソッド
+        public void AddScore(int amount)
+    {
+
+        // 「amount」に入ってくる数値分を加算していく。
+        score += amount;
+
+        scoreLabel.text = "Score " + score;
+    }
+}
